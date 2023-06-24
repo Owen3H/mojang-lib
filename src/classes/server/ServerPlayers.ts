a8d0153f72fc262107306758a67036a76a26404e
Owen Stubbs <owen77stubbs@gmail.com>
2023-06-24 17:06:54 +0100

    rename lib to src, remove docs.

--- src/classes/server/ServerPlayers.ts
+++ src/classes/server/ServerPlayers.ts
@@ -4,14 +4,14 @@ import Player, { PlayerIdentity } from '../player/Player'
 *  @class @desc Represents the players in a server
 */
 class ServerPlayers {
-  #max: number
-  #online: number
-  #sample: Player[]
+  readonly max: number
+  readonly online: number
+  readonly sample: Player[]
 
   constructor(data: ServerPlayerData) {
-    this.#max = data.max
-    this.#online = data.online
-    this.#sample = data.sample?.map(pl => new Player(pl))
+    this.max = data.max
+    this.online = data.online
+    this.sample = data.sample?.map(pl => new Player(pl))
   }
 }
 
