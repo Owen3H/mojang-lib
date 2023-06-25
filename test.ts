c8a1980782069bf3b3544d2120194dab79378a87
Owen Stubbs <owen77stubbs@gmail.com>
2023-06-25 19:18:30 +0100

    Delete test.ts

--- test.ts
+++
@@ -1,7 +0,0 @@
-import MCAPI from "./src/index.js"
-
-const server = await MCAPI.servers.get('play.earthmc.net')
-console.log(server)
-
-const player = await MCAPI.players.get('Owen3H')
-console.log(player)
